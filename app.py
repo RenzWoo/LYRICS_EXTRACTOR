615877af21936566f45dacd2649d9cf59ceec7c3
Renz <159403394+RenzWoo@users.noreply.github.com>
2025-12-31 22:56:27 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from pathlib import Path
 
-file = Path("WORTHY OF IT ALL.txt")
+folder = Path(r'C:\Users\User\Documents\Lyrics')
 
-print(file.read_text(encoding='utf-8'))
+records = []
 
+for idx, file in enumerate(folder.glob("*.txt")):
+    records.append({'id':idx,'song':file.name, 'lyrics':file.read_text(encoding='utf-8')})
+
+print(pd.DataFrame(records))
 
--- database.py
+++ database.py
@@ -1,16 +1,13 @@
 from sqlmodel import SQLModel, Field, create_engine, Session, select
-from pathlib import Path
 from typing import Optional
 import re
 
+postgres_url = "postgresql://postgres:admin@localhost:5432/lyricsdb"
+engine = create_engine(postgres_url)
+
 class Lyrics(SQLModel, table=True):
     id: int = Field(primary_key=True)
-    title: str
+    song: str
     lyrics: str
 
-
-DATABASE_URL = "sqlite://lyrics.db"
-engine = create_engine(DATABASE_URL, echo=False)
-
-def create_database():
-    SQLModel.metadata.create_all(engine)
+SQLModel.metadata.create_all(engine)
